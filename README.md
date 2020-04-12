a20034b8eaa89b8a838491f18d63308f14266043
Rebecca Lessem <beckognize@gmail.com>
2020-04-12 17:05:43 +0100

    Changed stuff MYOB

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # fortune5000
-to run locally type npm run dev in terminal
+to run locally type `npm run dev` in terminal
--- pages/open-cookie.js
+++ pages/open-cookie.js
@@ -1,11 +1,7 @@
-import Head from 'next/head';
-
 export default () => {
+    
     return (
       <div>
-        <Head>
-          <link href="/css/index.css" rel="stylesheet" />
-        </Head>
         <h1>This Is A Web Page Bitch!</h1>
         <p>This is not a good web page bitch.</p>
         <img src="/images/punkotter.jpg"></img>
