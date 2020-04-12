c4105537382dbcba8fc53cd9ebadb0520fa1129e
Rebecca Lessem <beckognize@gmail.com>
2020-04-12 16:00:29 +0100

    Correct spelling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Index() {
     return (
       <div>
-        <h1>This Is A Wep Page Bitch!</h1>
+        <h1>This Is A Web Page Bitch!</h1>
         <p>This is not a good web page bitch.</p>
         <img src="/images/punkotter.jpg"></img>
       </div>
