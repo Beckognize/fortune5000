1d36ecaf0a9cd71a92daf0c7ac68318591440d0f
Rebecca Lessem <beckognize@gmail.com>
2020-06-13 19:48:43 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 import Header from '../components/header';
 import Link from 'next/link'
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-
-export default function SimpleMediaQuery() {
-  const matches = useMediaQuery('(min-width:600px)');
-
-  return <span>{`(min-width:600px) matches: ${matches}`}</span>;
-}
 export default () => {
     return (
       
