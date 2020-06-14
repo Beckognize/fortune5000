024c5dfa0f4d7d76daf5d7ebd85f887edc33ea10
Rebecca Lessem <beckognize@gmail.com>
2020-06-14 16:19:15 +0100

    media query

--- pages/open-cookie.js
+++ pages/open-cookie.js
@@ -9,36 +9,36 @@ You will find the Shiba Inu of your dreams.
 You will be disappointed with this fortune.
 The fortune you seek is in another cookie.
 A closed mouth gathers no feet.
-A conclusion is simply the place where you got tired of thinking.
+A conclusion is simply the place where you stopped thinking.
 A cynic is only a frustrated optimist.
-A foolish man listens to his heart. A wise man listens to cookies.
+The fools listen to their hearts. The wise listen to cookies.
 You will die alone and poorly dressed.
-A fanatic is one who can't change his mind, and won't change the subject.
+A fanatic can't change his mind, and won't change the subject.
 If you look back, you'll soon be going that way.
 You will live long enough to open many fortune cookies.
-An alien of some sort will be appearing to you shortly.
+Duck!
 Do not mistake temptation for opportunity.
-Flattery will go far tonight.
+You will be spending a lot of time at home.
 He who laughs at himself never runs out of things to laugh at.
 He who laughs last is laughing at you.
 He who throws dirt is losing ground.
-Some men dream of fortunes, others dream of cookies.
+Some dream of fortunes, others dream of cookies.
 The greatest danger could be your stupidity.
 We don't know the future, but here's a cookie.
-The world may be your oyster, but it doesn't mean you'll get its pearl.
+I don't wan't the world, I just want your half.
 You will be hungry again in one hour.
 The road to riches is paved with homework.
 You can always find happiness at work on Friday.
 Actions speak louder than fortune cookies.
-Because of your melodic nature, the moonlight never misses an appointment.
+If you're not living on the edge, you're taking up too much room.
 Don't behave with cold manners.
 Don't forget you are always on our minds.
 Fortune not found? Abort, Retry, Ignore.
 Help! I am being held prisoner in a fortune cookie factory.
 It's about time I got out of that cookie.
 Never forget a friend. Especially if he owes you.
-Never wear your best pants when you go to fight for freedom.
-Only listen to the fortune cookie; disregard all other fortune telling units.
+Never wear your best pants while you fight for freedom.
+Only listen to the cookie; disregard all other fortune telling units.
 It is a good day to have a good day.
 All fortunes are wrong except this one.
 Someone will invite you to a Karaoke party.
